10c49c09134edfe56eada974de56a5832790d953
zack-erisyon <zack@erisyon.com>
2020-07-21 17:43:33 +0000

    wip on build ext

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 # python setup.py build_ext --inplace
 
+import sys
 import os
+import subprocess
 import pathlib
+from pathlib import Path
 from setuptools import Extension, dist, find_packages, setup
-from setuptools.command.build_ext import build_ext as build_ext_orig
+from setuptools.command.build_ext import build_ext
 
 dist.Distribution().fetch_build_eggs(["Cython>=0.15.1", "numpy>=1.10"])
 
@@ -57,49 +60,72 @@ extensions = [
 ]
 
 
-# CMake logic copied from https://stackoverflow.com/a/48015772
+# CMake logic copied from https://gist.github.com/ossareh/38ea13049d23f6742f78046170f4c033#file-setup-py-L86
 class CMakeExtension(Extension):
     def __init__(self, name):
-        # don't invoke the original build_ext for this special extension
-        super().__init__(name, sources=[])
+        import pudb; pudb.set_trace()
+        Extension.__init__(self, name, sources=[])
 
 
-class build_ext(build_ext_orig):
+class CMakeBuild(build_ext):
     def run(self):
-        for ext in self.extensions:
-            self.build_cmake(ext)
-        super().run()
+        import pudb; pudb.set_trace()
+        os.chdir("/erisyon/plaster/plaster/vendor/flann")
+        
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: " +
+                ", ".join(e.name for e in self.extensions))
 
-    def build_cmake(self, ext):
-        cwd = pathlib.Path().absolute()
+        build_directory = os.path.abspath(self.build_temp)
 
-        # these dirs will be created in build_py, so if you don't have
-        # any python sources to bundle, the dirs will be missing
-        build_temp = pathlib.Path(self.build_temp)
-        build_temp.mkdir(parents=True, exist_ok=True)
-        extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
-        extdir.mkdir(parents=True, exist_ok=True)
-
-        # example of cmake args
-        config = 'Debug' if self.debug else 'Release'
         cmake_args = [
-            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(extdir.parent.absolute()),
-            '-DCMAKE_BUILD_TYPE=' + config
+            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + build_directory,
+            '-DPYTHON_EXECUTABLE=' + sys.executable
         ]
 
-        # example of build args
-        build_args = [
-            '--config', config,
-            '--', '-j4'
-        ]
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
 
-        os.chdir(str(build_temp))
-        self.spawn(['cmake', str(cwd)] + cmake_args)
-        if not self.dry_run:
-            self.spawn(['cmake', '--build', '.'] + build_args)
-        # Troubleshooting: if fail on line above then delete all possible
-        # temporary CMake files including "CMakeCache.txt" in top level dir.
-        os.chdir(str(cwd))
+        cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+
+        # Assuming Makefiles
+        build_args += ['--', '-j2']
+
+        self.build_args = build_args
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get('CXXFLAGS', ''),
+            self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        # CMakeLists.txt is in the same directory as this setup.py file
+        cmake_list_dir = os.path.abspath(os.path.dirname(__file__))
+        print('-' * 10, 'Running CMake prepare', '-' * 40)
+        subprocess.check_call(['cmake', cmake_list_dir] + cmake_args,
+                              cwd=self.build_temp, env=env)
+
+        print('-' * 10, 'Building extensions', '-' * 40)
+        cmake_cmd = ['cmake', '--build', '.'] + self.build_args
+        subprocess.check_call(cmake_cmd,
+                              cwd=self.build_temp)
+
+        # Move from build temp to final position
+        for ext in self.extensions:
+            self.move_output(ext)
+
+    def move_output(self, ext):
+        import pudb; pudb.set_trace()
+        build_temp = Path(self.build_temp).resolve()
+        dest_path = Path(self.get_ext_fullpath(ext.name)).resolve()
+        source_path = build_temp / self.get_ext_filename(ext.name)
+        dest_directory = dest_path.parents[0]
+        dest_directory.mkdir(parents=True, exist_ok=True)
+        self.copy_file(source_path, dest_path)
 
 
 setup(
@@ -146,7 +172,7 @@ setup(
         "wheel",
         "zbs.zest",
     ],
+    cmdclass=dict(build_ext=CMakeBuild),
     python_requires=">=3.6",
-    #ext_modules=[CMakeExtension('./plaster/vendor/flann')] + cythonize(extensions, language_level="3"),
-    ext_modules=[CMakeExtension('flann')] #+ cythonize(extensions, language_level="3"),
+    ext_modules=[CMakeExtension('plaster.vendor.flann')] #+ cythonize(extensions, language_level="3"),
 )
