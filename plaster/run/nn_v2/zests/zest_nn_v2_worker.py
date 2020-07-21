4a18a4c5dff40b39be6aa93fa316e1cfad86e7e2
zack-erisyon <zack@erisyon.com>
2020-07-21 14:51:38 +0000

    WIP

--- plaster/run/nn_v2/zests/zest_nn_v2_worker.py
+++ plaster/run/nn_v2/zests/zest_nn_v2_worker.py
@@ -10,8 +10,9 @@ from plaster.run.sim_v2.sim_v2_params import SimV2Params, ErrorModel
 from plaster.tools.log.log import debug, prof
 from zest import zest
 
-@zest.skip("T")
+
 def zest_nn_v2_worker():
+    import pudb; pudb.set_trace()
     prep_params = PrepParams(proteins=[Munch(name="pep1", sequence="ABCDE")])
 
     pros = pd.DataFrame(
@@ -57,44 +58,6 @@ def zest_nn_v2_worker():
     sim_v2_result = sim_v2_worker.sim(sim_v2_params, prep_result)
 
     nn_v2_params = NNV2Params()
-
-    prep_params = PrepParams(proteins=[Munch(name="pep1", sequence="ABCDE")])
-
-    pros = pd.DataFrame(
-        dict(
-            pro_i=[1],
-            pro_id=["pep1"],
-            pro_is_decoy=[False],
-            pro_ptm_locs=[None],
-            pro_report=[None],
-        )
-    )
-
-    pro_seqs = pd.DataFrame(
-        dict(
-            aa=["A", "B", "C", "D", "E"],
-            pro_i=[1, 1, 1, 1, 1],
-            pro_name=["pep1", "pep1", "pep1", "pep1", "pep1"],
-            pro_ptm_locs=[None, None, None, None, None],
-            pro_report=[None, None, None, None, None],
-        )
-    )
-
-    prep_result = PrepResult(
-        params=prep_params,
-        _pros=pros,
-        _pro_seqs=pro_seqs,
-        _peps=peps,
-        _pep_seqs=pep_seqs,
-    )
-
-    error_model = ErrorModel.no_errors(n_channels=2)
-
-    sim_v2_params = SimV2Params.construct_from_aa_list(
-        ["A", "B"], error_model=error_model, n_edmans=4
-    )
-
-    sim_v2_result = sim_v2_worker.sim(sim_v2_params, prep_result)
 
     nn_v2_result = nn_v2_worker(nn_v2_params, sim_v2_result)
 
