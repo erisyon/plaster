1b027ce29ccba74d050fc4ef51c1b2270209e7a6
Zack <zack@erisyon.com>
2021-01-04 21:48:42 +0000

    wip

--- plaster/run/sigproc_v2/sigproc_v2_result.py
+++ plaster/run/sigproc_v2/sigproc_v2_result.py
@@ -168,6 +168,9 @@ class SigprocV2Result(BaseResult):
         except Exception as e:
             return "SigprocV2Result"
 
+    def limit(self, n_field_files=1):
+        self.field_files = self.field_files[0:n_field_files]
+
     def _cache(self, prop, val=None):
         # TASK: This might be better done with a yielding context
         cache_key = f"_load_prop_cache_{prop}"
